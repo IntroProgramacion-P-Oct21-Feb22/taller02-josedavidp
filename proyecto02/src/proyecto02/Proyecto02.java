b6a9264dcae7db48387a65db4d2d97d06cacf6a1
Jose <jdpiedra1@utpl.edu.ec>
2021-10-22 11:33:55 -0500

    primer commit

---
+++ proyecto02/src/proyecto02/Proyecto02.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto02;
+
+/**
+ *
+ * @author SALAA
+ */
+public class Proyecto02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Ejemplo dos básico de Java");
+        System.out.println("Versionado a través de Git");
+        System.out.println("Manejado desde GitHub");
+        System.out.println("Además se usar GitHubClassroom");
+    }
+    
+}
