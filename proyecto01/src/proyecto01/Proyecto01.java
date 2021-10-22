f5c5ea91c747eb99a1c934ff0ae29ad0360979cc
Jose <jdpiedra1@utpl.edu.ec>
2021-10-22 11:25:18 -0500

    primer commit

--- proyecto01/src/proyecto01/Proyecto01.java
+++ proyecto01/src/proyecto01/Proyecto01.java
@@ -16,9 +16,10 @@ public class Proyecto01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Ejemplo básico de Java");
-        System.out.println("Versionado a través de Git");
+        System.out.println("Ejemplo de Java");
         System.out.println("Manejado desde GitHub");
+        System.out.println("Versionado a través de Git");
+        System.out.println("Creado por josedavidp");
     }
     
 }
